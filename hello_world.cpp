c927c890c7a64466294963c7c333c2a231be7d11
nitsy13 <nnitss@mail.ru>
2025-03-24 18:13:37 +0300

    maked new form of hello_world.cpp to ask for name

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
-std::cout << "Hello world!";
+cout << "Hello world!";
+string name;
+cout << "Please enter username";
+cin >> name;
+cout << "Hello world from " << name;
 }
