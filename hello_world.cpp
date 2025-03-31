4c5c850bccbc4b113773e106c425a7093d40f269
nitsy13 <nnitss@mail.ru>
2025-03-31 19:38:29 +0300

    transformed codestyle

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
 
-int main() {
-std::cout << "Hello world!";
-std::string name;
-std::cout << "Please enter username";
-std::cin >> name;
-// привет привет привет
-std::cout << "Hello world from " << name;
+int
+main()
+{
+  std::cout << "Hello world!";
+  std::string name;
+  std::cout << "Please enter username";
+  std::cin >> name;
+  // привет привет привет
+  std::cout << "Hello world from " << name;
 }
